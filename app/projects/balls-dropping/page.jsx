2a8eca8eb09ac8e064b145ec30bc73501a251333
James Yuill <jamesyuill@jamess-mbp.lan>
2024-02-14 12:00:51 +0000

    replaced the project layout with a display grid system so that when the screen is past a certain size the layout changes to be more pleasing on the eye, and to also give the image more space on smaller screens

--- app/projects/balls-dropping/page.jsx
+++ app/projects/balls-dropping/page.jsx
@@ -9,38 +9,41 @@ export default function ballsDropping() {
     <main className={styles.main}>
       <PageHeading title={'Balls Dropping'} />
 
-      <div id="project-image" className={styles.imageandtech}>
-        <a href="https://balls-dropping.netlify.app/" target="_blank">
-          <img
-            src="/project-images/balls-dropping-desktop.png"
-            className={styles.projectimage}
-          />
-        </a>
+      <div className={styles.projectcontainer}>
+        {/* image div */}
+        <div id="project-image-div" className={styles.projectimagediv}>
+          <a href="https://balls-dropping.netlify.app/" target="_blank">
+            <img
+              src="/project-images/balls-dropping-desktop.png"
+              className={styles.projectimg}
+            />
+          </a>
+        </div>
 
-        <div id="project-tech" className={styles.projecttechandlinks}>
-          {/* map over project skills */}
+        {/* project links div */}
+        <div id="project-links" className={styles.projectlinks}>
+          <a
+            className={styles.plink}
+            href="https://github.com/jamesyuill/bouncing-ball"
+            target="_blank"
+          >
+            Github
+          </a>
 
-          <div id="project-links" className={styles.projectlinks}>
-            {/* githubs etc */}
-            <a
-              className={styles.plink}
-              href="https://github.com/jamesyuill/bouncing-ball"
-              target="_blank"
-            >
-              Github
-            </a>
+          <a
+            className={styles.plink}
+            href="https://balls-dropping.netlify.app/"
+            target="_blank"
+          >
+            Visit Site
+          </a>
+        </div>
 
-            <a
-              className={styles.plink}
-              href="https://balls-dropping.netlify.app/"
-              target="_blank"
-            >
-              Visit Site
-            </a>
-          </div>
+        {/* teck stack unordered list */}
+        <div id="tech-stack" className={styles.techstack}>
+          <p className={styles.textb}>Tech Stack:</p>
 
           <ul className={styles.unordered}>
-            <p className={styles.textb}>Tech Stack:</p>
             {techSkills.map((item) => {
               return (
                 <li key={item}>
@@ -50,30 +53,30 @@ export default function ballsDropping() {
             })}
           </ul>
         </div>
-      </div>
 
-      <div id="project-container" className={styles.projectcontainer}>
-        <p className={styles.subheading}>An Audio Visual App</p>
-        <div id="project-info" className={styles.projectinfo}>
-          <div id="description">
-            <p className={styles.text}>
-              - Built in vanilla Three.js and using Tone.js for the audio
-              elements.
-            </p>
-            <p className={styles.text}>
-              - The aim was to get a visual representation of a bouncing ball,
-              but once I'd built one ball, I then wanted to generate more. I
-              refactored the bouncing ball as an extended class with it's own
-              methods.
-            </p>
-            <p className={styles.text}>
-              - I would like to add the ability for a user to upload an audio
-              clip and for that to be used. Maybe even to split the sounds up
-              over the number of balls.
-            </p>
-            <p className={styles.text}>
-              - To learn more about the app visit the github links below.
-            </p>
+        <div id="title-and-description" className={styles.titleanddescription}>
+          <p className={styles.subheading}>An Audio Visual App</p>
+          <div id="project-info" className={styles.projectinfo}>
+            <div id="description">
+              <p className={styles.text}>
+                - Built in vanilla Three.js and using Tone.js for the audio
+                elements.
+              </p>
+              <p className={styles.text}>
+                - The aim was to get a visual representation of a bouncing ball,
+                but once I'd built one ball, I then wanted to generate more. I
+                refactored the bouncing ball as an extended class with it's own
+                methods.
+              </p>
+              <p className={styles.text}>
+                - I would like to add the ability for a user to upload an audio
+                clip and for that to be used. Maybe even to split the sounds up
+                over the number of balls.
+              </p>
+              <p className={styles.text}>
+                - To learn more about the app visit the github links below.
+              </p>
+            </div>
           </div>
         </div>
       </div>
