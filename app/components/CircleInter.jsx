d59a6c2ef1179f78aedeb19e2356cf7277c46cd5
James Yuill <jamesyuill@jamess-mbp.lan>
2024-03-01 18:18:02 +0000

    updates with a grid of circle svgs

--- app/components/CircleInter.jsx
+++ app/components/CircleInter.jsx
@@ -2,30 +2,38 @@
 import { useEffect, useState } from 'react';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
+import styles from './circleinter.module.css';
 
 export default function CircleInter() {
   const [windowSize, setWindowSize] = useState({
     width: undefined,
     height: undefined,
   });
+
+  let ballArray = [];
+
   useEffect(() => {
     setWindowSize({
       width: window.innerWidth,
       height: window.innerHeight,
     });
+
     // gsap.set('.ball', { xPercent: -50, yPercent: -50 });
     // let targets = gsap.utils.toArray('.ball');
+
+    // -------- create the ball grid ----------
   }, []);
 
   useGSAP(() => {
-    document.getElementById('ball').addEventListener('mouseenter', animate);
-    document.getElementById('ball').addEventListener('mouseleave', animateOut);
+    // document.getElementById('ball').addEventListener('mouseenter', animate);
+    // document.getElementById('ball').addEventListener('mouseleave', animateOut);
+
     function animate() {
       gsap.to(ball, {
         duration: 0.8,
-        x: 30,
-        y: 30,
-        // ease: 'power1.out',
+        x: gsap.utils.random(-10, 10),
+        y: gsap.utils.random(-10, 10),
+        ease: 'power1.out',
         // overwrite: 'auto',
         // stagger: 0.02,
       });
@@ -35,24 +43,40 @@ export default function CircleInter() {
         duration: 0.8,
         x: 0,
         y: 0,
-        // ease: 'power1.out',
+        ease: 'power1.out',
         // overwrite: 'auto',
         // stagger: 0.02,
       });
     }
   }, [windowSize]);
 
+  if (window.innerWidth) {
+    ballArray = [];
+    for (let y = 0; y < windowSize.height; y += 20) {
+      for (let x = 0; x < windowSize.width; x += 20) {
+        ballArray.push({
+          x: x,
+          y: y,
+        });
+      }
+    }
+  }
+
   return (
     <div>
-      <svg height={windowSize.height} width={windowSize.width}>
-        <circle
-          id="ball"
-          className="ball"
-          r={100}
-          cx={350}
-          cy={200}
-          fill="red"
-        />
+      <svg className={styles.svgcanvas}>
+        {ballArray.map((item, index) => {
+          return (
+            <circle
+              id="ball"
+              key={index}
+              r={10}
+              cx={item.x}
+              cy={item.y}
+              fill="red"
+            ></circle>
+          );
+        })}
       </svg>
     </div>
   );
