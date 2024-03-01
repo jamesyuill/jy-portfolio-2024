0e4c700b6124f0668066dadd0cc4ed5fc36251af
James Yuill <jamesyuill@jamess-mbp.lan>
2024-03-01 19:21:41 +0000

    updates with interactive svg circles

--- app/components/CircleInter.jsx
+++ app/components/CircleInter.jsx
@@ -11,6 +11,7 @@ export default function CircleInter() {
   });
 
   let ballArray = [];
+  const potentialColors = ['#0802A3', '#FF4B91', '#FF7676', '#FFCD4B'];
 
   useEffect(() => {
     setWindowSize({
@@ -27,36 +28,56 @@ export default function CircleInter() {
   useGSAP(() => {
     // document.getElementById('ball').addEventListener('mouseenter', animate);
     // document.getElementById('ball').addEventListener('mouseleave', animateOut);
+    const balls = gsap.utils.toArray('#ball');
 
-    function animate() {
+    balls.forEach((ball) => {
       gsap.to(ball, {
-        duration: 0.8,
-        x: gsap.utils.random(-10, 10),
-        y: gsap.utils.random(-10, 10),
+        duration: 1,
+        x: gsap.utils.random(-20, 20),
+        y: gsap.utils.random(-20, 20),
         ease: 'power1.out',
         // overwrite: 'auto',
         // stagger: 0.02,
       });
-    }
-    function animateOut() {
-      gsap.to(ball, {
-        duration: 0.8,
-        x: 0,
-        y: 0,
-        ease: 'power1.out',
-        // overwrite: 'auto',
-        // stagger: 0.02,
+
+      balls.forEach((ball) => {
+        ball.addEventListener('mouseenter', (e) => {
+          gsap.to(e.target, {
+            duration: 1,
+            x: gsap.utils.random(-20, 20),
+            y: gsap.utils.random(-20, 20),
+            r: 60,
+            ease: 'power1.out',
+
+            // overwrite: 'auto',
+            // stagger: 0.02,
+          });
+        });
+        ball.addEventListener('mouseleave', (e) => {
+          gsap.to(e.target, {
+            duration: 1,
+            x: gsap.utils.random(-20, 20),
+            y: gsap.utils.random(-20, 20),
+            r: 40,
+            ease: 'power1.out',
+            // overwrite: 'auto',
+            // stagger: 0.02,
+          });
+        });
       });
-    }
+    });
   }, [windowSize]);
 
   if (window.innerWidth) {
     ballArray = [];
-    for (let y = 0; y < windowSize.height; y += 20) {
-      for (let x = 0; x < windowSize.width; x += 20) {
+    for (let y = 0; y < windowSize.height; y += 60) {
+      for (let x = 0; x < windowSize.width; x += 60) {
+        let randomChoice = Math.floor(Math.random() * potentialColors.length);
+        let randomColor = potentialColors[randomChoice];
         ballArray.push({
           x: x,
           y: y,
+          color: randomColor,
         });
       }
     }
@@ -70,10 +91,10 @@ export default function CircleInter() {
             <circle
               id="ball"
               key={index}
-              r={10}
+              r={40}
               cx={item.x}
               cy={item.y}
-              fill="red"
+              fill={item.color}
             ></circle>
           );
         })}
