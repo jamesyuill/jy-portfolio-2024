fc37cd2eca73692d302c1d7bebf6af5db7db25ac
James Yuill <jamesyuill@jamess-mbp.lan>
2024-03-09 19:13:35 +0000

    changes the way the env variables are stored and accessed

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -21,7 +21,7 @@ export default function Contact() {
     emailaddress: '',
     message: '',
   });
-  emailjs.init(process.env.VITE_EMAILJS_PUBLIC_KEY);
+  emailjs.init(process.env.EMAILJS_PUBLIC_KEY);
 
   const responseObj = {
     0: '',
@@ -34,10 +34,10 @@ export default function Contact() {
 
     emailjs
       .sendForm(
-        process.env.VITE_EMAILJS_SERVICE_ID,
-        process.env.VITE_EMAILJS_TEMPLATE_ID,
+        process.env.EMAILJS_SERVICE_ID,
+        process.env.EMAILJS_TEMPLATE_ID,
         form.current,
-        process.env.VITE_EMAILJS_PUBLIC_KEY
+        process.env.EMAILJS_PUBLIC_KEY
       )
       .then(
         (result) => {
