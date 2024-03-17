192aba9b4a8bf5119fedfb08b0c93f24bf1d51df
James Yuill <jamesyuill@jamess-mbp.lan>
2024-03-17 20:14:21 +0000

    removes the white page that is not needed

--- app/components/WhitePage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './whitepage.module.css';
-
-export default function WhitePage({ close, setClose }) {
-  return (
-    <div className={close ? styles.closewhitepage : styles.showwhitepage}></div>
-  );
-}
