a6f4652a6a1be7d95e5ccb03021893a19f351173
James Yuill <jamesyuill@jamess-mbp.lan>
2024-03-17 17:20:15 +0000

    updates the project card to have a next image component load the image

--- app/components/ProjectCard.jsx
+++ app/components/ProjectCard.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import styles from './projectcard.module.css';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function ProjectCard({ title, imgUrl, disc, endpoint }) {
   return (
     <Link href={`projects/${endpoint}`} className={styles.link}>
       <article className={styles.article}>
-        <img className={styles.img} src={`project-images/${imgUrl}`} />
+        <div className={styles.imagecontainer}>
+          <Image
+            src={`/project-images/${imgUrl}`}
+            width={300}
+            height={250}
+            alt={title}
+            placeholder="empty"
+            style={{
+              width: '100%',
+              height: '100%',
+              objectFit: 'contain',
+            }}
+          />
+        </div>
+
+        {/* <img className={styles.img} src={`project-images/${imgUrl}`} /> */}
         <div>
           <h1 className={styles.h1}>{title}</h1>
           <h2 className={styles.h2}>{disc}</h2>
